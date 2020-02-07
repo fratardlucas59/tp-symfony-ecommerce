ad0d7cf0d7b71436d6d913a57fc5f7b79c6fd332
fratardlucas59 <fratardlucasdevweb@gmail.com>
2020-02-07 12:05:03 +0100

    page catégorie + filtres

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,4 +18,29 @@ class CategoryController extends AbstractController
             'controller_name' => 'CategoryController',
         ]);
     }
+
+  /**
+   * @Route("/category/{slug}", name="category_show")
+   */
+  public function show($slug, ProductRepository $productRepository){
+    $categoryRepository = $this->getDoctrine()->getRepository(Category::class);
+
+    $products = $productRepository->findAll();
+
+    $last = $productRepository->last();
+
+    $categories = $categoryRepository->categories();
+    $category = $categoryRepository->findOneBySlug($slug);
+
+    if (!$category) {
+      throw $this->createNotFoundException('La catégorie n\'existe pas.');
+    }
+
+    return $this->render('category/show.html.twig', [
+      'category' => $category,
+      'products' => $products,
+      'last' => $last,
+      'categories' => $categories
+    ]);
+  }
 }
